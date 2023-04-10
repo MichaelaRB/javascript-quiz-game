60fea62d0e2a05ceb2a06d6a0f63a19758812f8f
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-09 21:54:16 -0500

    added the base html elements, styled the buttons and the fonts, and used javascript to hide certain elements on the start screen

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var startElements = document.querySelector(".startScreen");
+var gameElements = document.querySelector(".gameScreen");
+var resultsElements = document.querySelector(".resultScreen");
+var answer1 = document.querySelector("#answer1");
+var answer2 = document.querySelector("#answer2");
+var answer3 = document.querySelector("#answer3");
+var answer4 = document.querySelector("#answer4");
+
+gameElements.setAttribute("style","display: none");
+resultsElements.setAttribute("style","display: none");
