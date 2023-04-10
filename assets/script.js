22b183cef538285ea97e2f93c240d9dd4a8e64a6
Michaela Brown <michaelarbrown4@gmail.com>
2023-04-09 22:36:55 -0500

    ammended event listener to start button so that the fields would be populated with the first question and answers

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,25 @@ var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
 var answer4 = document.querySelector("#answer4");
+var answers = [answer1, answer2, answer3, answer4];
+
+var question1 = {
+    question: "Which of the following is the correct way to declare and initialize a variable in JavaScript?",
+    answers: ["var x = 10;","int x = 10;","variable x = 10;","--x: 10;"],
+    correctAns: "var x = 10;",
+};
+
+var question2 = {
+    question: "Which of the following is NOT a JavaScript data type?",
+    answers: ["Number","BigInt","Integer","String"],
+    correctAns: "Integer",
+};
+
+var question3 = {
+    question: "What will be logged to the console from the code console.log(typeof '9')",
+    answers: ["Number", "String", "Undefined","Boolean"],
+    correctAns: "String",
+};
 
 gameElements.setAttribute("style","display: none");
 resultsElements.setAttribute("style","display: none");
@@ -13,4 +32,10 @@ startElements.addEventListener("click", function(event) {
     if(!event.currentTarget==="button") return;
     startElements.setAttribute("style","display: none");
     gameElements.setAttribute("style","display: in-line");
+    gameElements.firstChild.textContent = question1.question;
+    for(var i = 0; i < answers.length; i++){
+        answers[i].textContent = question1.answers[i];
+    }
 });
+
+console.log(typeof '9');
